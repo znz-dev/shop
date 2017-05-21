b5e2fa70116a57b791eb70f9602ab07417e292dd
zhouxin <znz.zhouxin@gmail.com>
2017-05-22 00:11:38 +0800

    fix

--- src/main/java/org/znz/controller/PosterController.java
+++ src/main/java/org/znz/controller/PosterController.java
@@ -3,9 +3,13 @@ package org.znz.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.znz.dto.common.View;
 import org.znz.entity.Poster;
 import org.znz.service.PosterService;
+
+import java.io.File;
+import java.io.IOException;
 
 @CrossOrigin
 @Controller
@@ -17,7 +21,15 @@ public class PosterController {
 
     @RequestMapping(value = "/new", method = RequestMethod.POST, produces = {"application/json; charset=UTF-8"})
     @ResponseBody
-    public View create(@ModelAttribute Poster poster) {
+    public View create(@ModelAttribute Poster poster,
+                       @RequestParam("file") CommonsMultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            String rootPath = System.getProperty("shop.webRootPath");
+            String filePath = "/upload/" + file.getOriginalFilename();
+            String path = rootPath + filePath;
+            file.transferTo(new File(path));
+            poster.setPicture(filePath);
+        }
         return posterService.createPosterByParams(poster);
     }
 
